011884afd8552ad10081c7587fb784d7255d33ae
takezawahiroshi <takezawahiroshi@gmail.com>
2023-12-31 06:16:58 +0000

    Reset Git cache

---
+++ myapp/frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyButton from '../app/components/Button';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>udemy-aws-container</h1>
+      <MyButton />
+      <p id="message">ボタンをクリックしてください</p>
+    </div>
+  );
+};
+
+export default Home;
