7c05b3e418ec9ee0fd44ec92266889ca9dddc506
takezawahiroshi <takezawahiroshi@gmail.com>
2024-01-01 11:16:21 +0900

    test deploy

--- myapp/frontend/src/app/page.tsx
+++ myapp/frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import MyButton from '../app/components/Button';
 const Home = () => {
   return (
     <div>
-      <h1>udemy-aws-container</h1>
+      <h1>デプロイテスト</h1>
       <MyButton />
       <p id="message">ボタンをクリックしてください</p>
     </div>
