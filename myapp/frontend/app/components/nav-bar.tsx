e9d874bf1e9e4a6b0b1f7df644d5b3c6e9b4d139
takezawahiroshi <takezawahiroshi@gmail.com>
2024-01-04 00:12:40 +0900

    Add App width supabase

---
+++ myapp/frontend/app/components/nav-bar.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <header className="bg-gray-800 p-4">
+      <nav className="space-x-4">
+        <Link
+          href="/"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          Home
+        </Link>
+        <Link
+          href="/blogs"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          Nested Layout with Blogs
+        </Link>
+        <Link
+          href="/streaming-sr"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          Streaming SR
+        </Link>
+        <Link
+          href="/auth"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          Auth with CRUD
+        </Link>
+        <Link
+          href="/health-check"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          RDS
+        </Link>
+      </nav>
+    </header>
+  )
+}
