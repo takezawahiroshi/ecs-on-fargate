07995af1424ba6c9c24961fdafa17c0dfb3e81e4
takezawahiroshi <takezawahiroshi@gmail.com>
2024-01-04 17:44:57 +0900

    Update app

--- myapp/frontend/app/page.tsx
+++ myapp/frontend/app/page.tsx
@@ -7,7 +7,6 @@ import RefreshBtn from './components/refresh-btn'
 export default function Page() {
   return (
     <main>
-      <p>test16:57</p>
       <div className="m-10 text-center">
         <Suspense fallback={<Spinner color="border-green-500" />}>
           <NotesList />
